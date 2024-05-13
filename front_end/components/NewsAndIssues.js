602cbf3bd6198bb3f2c61d69ca5bb86a6ea48c45
SeungJun Seok <163656455+Seoksj@users.noreply.github.com>
2024-05-13 20:16:22 +0900

    뉴스and이슈 수정
    * 뉴스and이슈 수정
    
    * .idea 제거
    
    * 뉴스and이슈 수정

--- front_end/components/NewsAndIssues.js
+++ front_end/components/NewsAndIssues.js
@@ -1,30 +1,68 @@
-// 예시 코드 - 프론트 미구현
-
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 
 function NewsAndIssues() {
-  const [newsAndIssues, setNewsAndIssues] = useState([]);
+  const [news, setNews] = useState([]);
+  const [issues, setIssues] = useState([]);
+  const [activeCategory, setActiveCategory] = useState('news'); // 활성화된 카테고리를 추적하는 상태 변수
 
   useEffect(() => {
-    axios.get('/api/news_issues')
-      .then(response => {
-        setNewsAndIssues(response.data);
-      })
-      .catch(error => {
-        console.error('Error fetching news and issues:', error);
-      });
+    // Django에서 제공하는 API 엔드포인트
+    const apiUrl = '/api/news-and-issues';
+
+    // 데이터를 가져오는 함수
+    const fetchData = async () => {
+      try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        setNews(data.news);
+        setIssues(data.issues);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    // 데이터 가져오기
+    fetchData();
   }, []);
 
+  const handleCategoryClick = (category) => {
+    setActiveCategory(category);
+  };
+
+  const handleItemClick = (link) => {
+    window.open(link, '_blank'); // 새 탭에서 링크 열기
+  };
+
   return (
-    <div className="news-and-issues">
-      <h2>News & Issues</h2>
-      <ul>
-        {newsAndIssues.map(item => (
-          <li key={item.title}>{item.title}</li>
-        ))}
-      </ul>
+    <div>
+      <div>
+        <h3>뉴스</h3>
+        <button onClick={() => handleCategoryClick('news')}>뉴스 보기</button>
+        {activeCategory === 'news' && (
+          <ul>
+            {news.map((item, index) => (
+              <li key={index}>
+                <button onClick={() => handleItemClick(item.link)}>{item.title}</button>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+      <div>
+        <h3>다음 이슈</h3>
+        <button onClick={() => handleCategoryClick('issues')}>다음 이슈 보기</button>
+        {activeCategory === 'issues' && (
+          <ul>
+            {issues.map((item, index) => (
+              <li key={index}>
+                <button onClick={() => handleItemClick(item.link)}>{item.title}</button>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
     </div>
   );
 }
+
 export default NewsAndIssues;
