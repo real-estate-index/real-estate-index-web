875e8e854260f978697ca56de0e8e8924d3cde93
skytin1004 <skytin1004@gmail.com>
2024-05-13 21:08:58 +0900

    react package 버전 확정 및 전반적인 프론트 설계 진행 (아직 서버 실행은 안됨)

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,6 +1,9 @@
+// App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Map from './components/Map';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Map from './components/Map';  // 예제로 추가된 다른 컴포넌트
 import FearGreedIndex from './components/FearGreedIndex';
 import RegionDetails from './components/RegionDetails';
 import Graph from './components/Graph';
@@ -8,23 +11,13 @@ import NewsAndIssues from './components/NewsAndIssues';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <header className="App-header">
-          <h1>Real Estate Index</h1>
-        </header>
-        <main>
-          <div className="dashboard">
-            <FearGreedIndex />
-            <Map />
-            <RegionDetails />
-            <NewsAndIssues />
-            <Graph title="Graph1" />
-            <Graph title="Graph2" />
-          </div>
-        </main>
-      </div>
-    </Router>
+    <Routes>
+      <Route path="/map" element={<Map />} />
+      <Route path="/index" element={<FearGreedIndex />} />
+      <Route path="/details" element={<RegionDetails />} />
+      <Route path="/graph" element={<Graph />} />
+      <Route path="/news" element={<NewsAndIssues />} />
+    </Routes>
   );
 }
 
--- front_end/src/index.js
+++ front_end/src/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css'; // 전역 스타일 시트
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
