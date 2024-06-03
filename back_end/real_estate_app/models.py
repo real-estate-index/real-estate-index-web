c12e72631ba1e9a51c7498b49effff009c060ab0
skytin1004 <skytin1004@gmail.com>
2024-06-03 23:54:54 +0900

    구 목록 표시 기능 프론트 백엔드 초기구현 데이터 x

--- back_end/real_estate_app/models.py
+++ back_end/real_estate_app/models.py
@@ -24,7 +24,7 @@ class Region(models.Model):
     def __str__(self):
         return self.name
 
-# 지역별 구 조회
+# 지역별 구 조회(서울: 강남구, 강동구, 강북구, 강서구, 관악구, 광진구, 구로구, 금천구, 노원구, 도봉구, 동대문구, 동작구, 마포구, 서대문구, 서초구, 성동구, 성북구, 송파구, 양천구, 영등포구, 용산구, 은평구, 종로구, 중구, 중랑구)
 class District(models.Model):
     name = models.CharField(max_length=100)
     region = models.ForeignKey(Region, related_name='districts', on_delete=models.CASCADE)
